a9c4cf67ef37ce0854597dfeec47082b32c2e0f7
Zain Bacchus <zainbacchus@gmail.com>
2026-05-16 10:50:38 -0500

    Update sitemap.js

--- api/sitemap.js
+++ api/sitemap.js
@@ -1,14 +1,11 @@
 export const config = { runtime: 'edge' };
-
 export default function handler() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-  <url><loc>https://trackratsprint.club</loc></url>
-  <url><loc>https://trackratsprint.club/brand</loc></url>
-  <url><loc>https://shop.trackratsprint.club</loc></url>
-  <url><loc>https://trackratsprint.club/llms.txt</loc></url>
+  <url><loc>https://www.trackratsprint.club/</loc></url>
+  <url><loc>https://www.trackratsprint.club/2026-invitational</loc></url>
+  <url><loc>https://www.trackratsprint.club/brand</loc></url>
 </urlset>`;
-
   return new Response(xml, {
     headers: { 'content-type': 'application/xml' },
   });
